22a43e8f2d92ab249f8b1c58650b18085b8f0f9e
Omar Bravo <ojbravo@gmail.com>
2018-06-07 15:25:07 -0600

    Profile Name on Dashboard Functions Properly

--- Posifocus/Controllers/DashboardViewController.swift
+++ Posifocus/Controllers/DashboardViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UITextFieldDelegate {
     
     
     var cell: UITableViewCell?
@@ -17,30 +17,71 @@ class DashboardViewController: UIViewController {
     var profiles: Results<Profile>?
     let profile = Profile()
     
+    lazy var realm:Realm = {
+        return try! Realm()
+    }()
     
+    
+    @IBOutlet weak var profileName: UITextField!
     @IBOutlet weak var gratituteCount: UILabel!
     @IBOutlet weak var tasksCompleted: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        let realm = try! Realm()
-        
         if (realm.objects(Profile.self).count == 0) {
             try! realm.write {
                 realm.add(profile)
             }
         }
+        
         profiles = realm.objects(Profile.self)
+        
+        profileName.backgroundColor = UIColor.clear
+        profileName.borderStyle = .none
+        profileName.textColor = UIColor.white
+        profileName.text = "FirstName LastName"
+        profileName.delegate = self
+        profileName.text = profiles![0].name
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         
         gratituteCount.text = String(realm.objects(Gratitude.self).count)
         tasksCompleted.text = String(profiles![0].tasksCompleted)
+    }
+    
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if (profileName.isFirstResponder == true && profiles![0].name == "") {
+            profileName.text = ""
+        }
+        textField.textColor = UIColor.white
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if (textField.text?.isEmpty)! {
+            textField.text = "FirstName LastName"
+            textField.textColor = UIColor.lightGray
+            do {
+                try realm.write {
+                    profiles![0].name = ""
+                }
+            } catch {
+                print("Error writing empty profile name to Realm, \(error)")
+            }
+        } else {
+            
+            do {
+                try realm.write {
+                    profiles![0].name = profileName.text!
+                }
+            } catch {
+                print("Error writing profile name to Realm, \(error)")
+            }
+            textField.textColor = UIColor.white
+        }
     }
     
     
